fb74d5d431784ed45db4cd197eead711bb5f4cef
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-07-31 06:53:25 +0700

    no message

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def webook():
 
 
 def service(mode, user_id, message):
-    response = requests.get('http://521504a0.ngrok.io', {'mode': mode, 'user_id': user_id, 'message': message})
+    response = requests.get('http://521504a0.ngrok.io/process', {'mode': mode, 'user_id': user_id, 'message': message})
     if response.status_code != 200:
         return None
     response = json.loads(response.content)
