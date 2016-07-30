01fe8c85083e67580db8d542558dd96f42d6cddb
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-07-31 01:06:15 +0700

    no message

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def show_sug_buttons(recipient_id,sug_text):
                         {
                             "type":"postback",
                             "title": u"Đau tim",
-                            "payload":{"text" = "ok"}
+                            "payload":{"text" : "ok"}
                          },
                          {
                             "type":"postback",
