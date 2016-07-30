26f9cc946dabcda7da3a4dc849cb71a82fda804d
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-07-30 17:45:43 +0700

    adda

--- app.py
+++ app.py
@@ -75,15 +75,14 @@ def send_message(recipient_id, message_text):
         }
     })
     
-    
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
 
 def send_typing(recipient_id):
-    message_text = "haha"
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    #message_text = "haha"
+    log("sending message to {recipient}".format(recipient=recipient_id))
 
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
@@ -103,6 +102,8 @@ def send_typing(recipient_id):
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
+
+
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
