920154e158ddc85967b9248914adb2cce066fbaa
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-07-30 16:46:01 +0700

    dang khung`

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def webook():
         for entry in data["entry"]:
 
             for messaging_event in entry["messaging"]:
-                send_message(sender_id, type(messaging_event))
                 if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
