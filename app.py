0517e68c50bc19410a98f5a968d863aca53881e1
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-07-31 09:21:16 +0700

    asd

--- app.py
+++ app.py
@@ -98,19 +98,14 @@ def send_map(recipient_id, message):
         r.append({
                     'title': title,
                     'subtitle':subtitle,
-                    'image_url': 'http://staticmap.openstreetmap.de/staticmap.php?center=' + latitude + ',' + longitude + '&zoom=18&size=640x480&maptype=osmarenderer&markers=' + latitude + ',' + longitude + ',ol-marker',
+                    'image_url': 'http://staticmap.openstreetmap.de/staticmap.php?center=' + latitude + ',' + longitude + '&zoom=18&size=640x480&markers=' + latitude + ',' + longitude + ',ol-marker',
                     #'image_url' : 'http://staticmap.openstreetmap.de/staticmap.php?center=10.762952,106.682340&zoom=15&size=640x480&markers=10.762952,106.682340,ol-marker',
                     'buttons': [{
                         'type': 'web_url',
-                        'url': 'http://choray.vn/',
-                        'title': u'xem thông tin'
-                    }, {
-                        'type': 'web_url',
-                        'url': 'http://staticmap.openstreetmap.de',
-                        'title': u'hướng dẫn đường'
+                        'url': 'http://staticmap.openstreetmap.de/staticmap.php?center=' + latitude + ',' + longitude + '&zoom=18&size=640x480&markers=' + latitude + ',' + longitude + ',ol-marker',
+                        'title': u'Hướng dẫn đường'
                     }
                 ]})
-
 
     log(r)
 
