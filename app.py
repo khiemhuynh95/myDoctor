65ef37b43d67697274fde250abce8503e8128c48
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-08-19 20:34:18 +0700

    no message

--- app.py
+++ app.py
@@ -44,7 +44,6 @@ def webook():
                         
                         send_message(sender_id, "Hello")
                        
-                        
                     except:
                         pass
                 if messaging_event.get("delivery"):  # delivery confirmation
